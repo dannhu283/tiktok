72a6ceab510b1c306cd4df0627e57d830f1311d9
dannhu283 <dannhu2803@gmail.com>
2023-09-13 21:28:16 +0700

    add useEffect

--- src/Content.js
+++ src/Content.js
@@ -1,7 +1,42 @@
+import { useEffect, useState } from "react";
+
+//1. useEffect(callback)
+//-gọi callback mỗi khi component re-render
+//-goọi callback sau khi component sau khi thêm element vào dom
+
+//2. useEffect(callback,[])
+//- chỉ gọi callback 1 lần sau khi component được mounted
+
+//3. useEffect(callback,[deps])
+//-
+
+//----------------------
+//1.callback luôn được gọi sau khi component mounted
+
 function Content() {
+  const [title, setTitle] = useState("");
+
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((res) => res.json())
+      .then((posts) => {
+        setPosts(posts);
+      });
+
+    document.title = title;
+  }, []);
+
   return (
     <div>
-      <h1>Hi Bro</h1>
+      <input value={title} onChange={(e) => setTitle(e.target.value)} />
+
+      <ul>
+        {posts.map((post) => {
+          return <li key={post.id}>{post.title}</li>;
+        })}
+      </ul>
     </div>
   );
 }

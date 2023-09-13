ca27eb2193a7b595d3a6130a428782ad424c21bf
dannhu283 <dannhu2803@gmail.com>
2023-09-13 22:08:08 +0700

    add useEffect with dom event

--- src/Content.js
+++ src/Content.js
@@ -12,46 +12,86 @@ import { useEffect, useState } from "react";
 
 //----------------------
 //1.callback luôn được gọi sau khi component mounted
+//2.cleanup function luôn được gọi trước khi component unmounted
 
-const tabs = ["posts", "comments", "albums"];
+// const tabs = ["posts", "comments", "albums"];
 
 function Content() {
-  const [title, setTitle] = useState("");
+  //   const [title, setTitle] = useState("");
 
-  const [posts, setPosts] = useState([]);
+  //   const [posts, setPosts] = useState([]);
 
-  const [type, setType] = useState("posts");
+  //   const [type, setType] = useState("posts");
+
+  //   const [showGoToTop, setShowGoToTop] = useState(false);
+
+  // useEffect(() => {
+  //   fetch(`https://jsonplaceholder.typicode.com/${type}`)
+  //     .then((res) => res.json())
+  //     .then((posts) => {
+  //       setPosts(posts);
+  //     });
+  //   // console.log("type changed");
+
+  //   // document.title = title;
+  // }, [type]);
+
+  // useEffect(() => {
+  //   const handleScroll = () => {
+  //     if (window.scrollY >= 200) {
+  //       setShowGoToTop(true);
+  //     } else {
+  //       setShowGoToTop(false);
+  //     }
+  //   };
+  //   window.addEventListener("scroll", handleScroll);
+  //   //cleanup function
+  //   return () => {
+  //     window.removeEventListener("scroll", handleScroll);
+  //   };
+  // }, []);
+
+  const [width, setWidth] = useState(window.innerWidth);
 
   useEffect(() => {
-    fetch(`https://jsonplaceholder.typicode.com/${type}`)
-      .then((res) => res.json())
-      .then((posts) => {
-        setPosts(posts);
-      });
-    // console.log("type changed");
+    const handleReSize = () => {
+      setWidth(window.innerWidth);
+    };
 
-    // document.title = title;
-  }, [type]);
+    window.addEventListener("resize", handleReSize);
+
+    return () => {
+      window.removeEventListener("resize", handleReSize);
+    };
+  }, []);
 
   return (
-    <div>
-      {tabs.map((tab) => (
-        <button
-          key={tab}
-          onClick={() => setType(tab)}
-          // style={type === tab ? { color: "#fff" } : { color: "#333" }}
-        >
-          {tab}
-        </button>
-      ))}
-      <hr />
-      <input value={title} onChange={(e) => setTitle(e.target.value)} />
+    // <div>
+    //   {/* {tabs.map((tab) => (
+    //     <button
+    //       key={tab}
+    //       onClick={() => setType(tab)}
+    //       style={type === tab ? { color: "#fff" } : { color: "#333" }}
+    //     >
+    //       {tab}
+    //     </button>
+    //   ))}
+    //   <hr />
+    //   <input value={title} onChange={(e) => setTitle(e.target.value)} />
 
-      <ul>
-        {posts.map((post) => {
-          return <li key={post.id}>{post.title || post.name}</li>;
-        })}
-      </ul>
+    //   <ul>
+    //     {posts.map((post) => {
+    //       return <li key={post.id}>{post.title || post.name}</li>;
+    //     })}
+    //   </ul>
+    //   {showGoToTop && (
+    //     <button style={{ position: "fixed", right: 20, bottom: 20 }}>
+    //       BTT
+    //     </button>
+    //   )} */}
+    // </div>
+    <div>
+      <h1>{width}</h1>
     </div>
   );
 }
